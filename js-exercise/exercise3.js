26b4df18fb8b9793efffe35c198ff888a9665c41
st-phuongnguyen2 <phuong.nguyen2@supremetech.vn>
2025-04-07 23:01:07 +0700

    refactor code

--- js-exercise/exercise3.js
+++ js-exercise/exercise3.js
@@ -4,84 +4,58 @@ function paginate(totalPages, currentPage) {
   }
 
   const maxDisplay = 7;
-  const maxMargin = maxDisplay - 3;
+  const maxMargin = 4;
 
   const rightDistance = Math.abs(totalPages - currentPage);
   const leftDistance = Math.abs(1 - currentPage);
 
-  if (totalPages >= maxDisplay) {
+  const pagination = [];
+
+  if (totalPages > maxDisplay) {
     if (rightDistance < leftDistance && rightDistance < maxMargin) {
-      console.log('HERE rightDistance and three dots on the left');
-      return Array.from({ length: maxDisplay }, (item, i) => {
-        if (i >= 2) {
-          const page = totalPages - maxMargin - 1 + i;
-          return {
-            page,
-            isCurrent: currentPage === page,
-            showDot: false
-          };
-        } else {
-          const isDot = totalPages > maxDisplay && i === 1;
-          return {
-            page: isDot ? '...' : i + 1,
-            isCurrent: false,
-            showDot: isDot === 1
-          };
-        }
-      });
+      pagination.push({ page: 1, isCurrent: false, showDots: false });
+      pagination.push({ page: '...', isCurrent: false, showDots: true });
+
+      for (let i = 2; i < maxDisplay; i++) {
+        pagination.push({
+          page: i,
+          isCurrent: currentPage === i,
+          showDots: false
+        });
+      }
     } else if (leftDistance < rightDistance && leftDistance < maxMargin) {
-      console.log('HERE leftDistance and three dots on the right');
-      return Array.from({ length: maxDisplay }, (item, i) => {
-        if (i === 0) {
-          return {
-            page: 1,
-            isCurrent: currentPage === 1,
-            showDot: false
-          };
-        } else if (i === maxDisplay - 1) {
-          return {
-            page: totalPages,
-            isCurrent: currentPage === totalPages,
-            showDot: false
-          };
-        } else {
-          const isDot = totalPages > maxDisplay && i === maxDisplay - 2;
-          return {
-            page: isDot ? '...' : i + 1,
-            isCurrent: currentPage === i + 1,
-            showDot: isDot
-          };
-        }
+      for (let i = 1; i <= maxDisplay - 2; i++) {
+        pagination.push({
+          page: i,
+          isCurrent: currentPage === i,
+          showDots: false
+        });
+      }
+
+      pagination.push({ page: '...', isCurrent: false, showDots: true });
+      pagination.push({
+        page: totalPages,
+        isCurrent: false,
+        showDots: false
       });
     } else {
-      console.log('HERE middle and three dots on both left and right side');
-      return Array.from({ length: maxDisplay }, (item, i) => {
-        if (i === 0) {
-          return {
-            page: 1,
-            isCurrent: currentPage === 1,
-            showDot: false
-          };
-        } else if (i === maxDisplay - 1) {
-          return {
-            page: totalPages,
-            isCurrent: currentPage === totalPages,
-            showDot: false
-          };
-        } else if (i === 1 || i === maxDisplay - 2) {
-          return {
-            page: '...',
-            isCurrent: false,
-            showDot: true
-          };
-        } else {
-          const page = currentPage - 3 + i;
-          return {
-            page,
-            isCurrent: currentPage === page,
-            showDot: false
-          };
-        }
+      pagination.push({ page: 1, isCurrent: false, showDots: false });
+      pagination.push({ page: '...', isCurrent: false, showDots: true });
+
+      for (let i = 0; i < maxDisplay - 4; i++) {
+        const page = currentPage - 1 + i;
+        pagination.push({
+          page,
+          isCurrent: page === currentPage,
+          showDots: false
+        });
+      }
+
+      pagination.push({ page: '...', isCurrent: false, showDots: true });
+      pagination.push({
+        page: totalPages,
+        isCurrent: false,
+        showDots: false
       });
     }
   } else {
@@ -93,6 +67,8 @@ function paginate(totalPages, currentPage) {
       };
     });
   }
+
+  return pagination;
 }
 
-console.log('exercise 3:', paginate(10, 4));
+console.log('exercise 3:', paginate(3, 2));
